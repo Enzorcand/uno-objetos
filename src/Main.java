3d6e68aa6c799f89963fb3a756d1f56d3a9dbad4
Enzorcand <enzorcand@gmail.com>
2023-08-28 17:31:36 -0300

    Metodo que gera o baralho funcionando completamente, porem não consegue gerar o zero verde por algum motivo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Deck baralho = new Deck(1);
+        for (int i = 0; i < baralho.getCards().length; i++){
+            if(baralho.getCard(i).getIsJoker()){
+                System.out.println(baralho.getCard(i).getType());
+            } else {
+                System.out.print(baralho.getCard(i).getColor() + " ");
+                System.out.println(baralho.getCard(i).getType());
+            }
+        }
+
     }
 }
