3df42ea94eec919db2f60fa15d827fca0563396b
Enzorcand <enzorcand@gmail.com>
2023-10-21 13:51:32 -0300

    Código utilizado para testar a implementação dos novos métodos.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Hand> jogadores = new ArrayList<>();
         Deck baralho = new Deck(1);
-        for (Card card : baralho.getCards()){
-            if(card instanceof Joker){
-                System.out.println(card.getType());
-            } else {
-                System.out.print(card.getType() + " ");
-                System.out.println(card.getColor());
-            }
+
+        System.out.println("Insira o numero de jogadores:");
+        int n = scan.nextInt();
+        for(int i = 0; i < n; i++){
+            Hand h = new Hand(baralho);
+            jogadores.add(h);
         }
     }
 }
