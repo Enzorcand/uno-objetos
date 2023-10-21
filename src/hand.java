7a314c173579fdb84145c2ff00c66530badf87a0
Enzorcand <enzorcand@gmail.com>
2023-10-21 13:50:23 -0300

    implementação da classe Hand, responsável por comprar a mão inicial do jogador..

--- src/hand.java
+++ src/hand.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
 public class Hand {
+    ArrayList<Card> pHand;
+    public Hand(Deck deck){
+        setHand(deck);
+    }
+
+    //Metodo que gera a mão inicial de cada jogador
+    public void setHand(Deck deck){
+        for(int i = 0; i < 7; i++){
+            Card card = deck.buyCard();
+            pHand.add(card);
+        }
+    }
 }
